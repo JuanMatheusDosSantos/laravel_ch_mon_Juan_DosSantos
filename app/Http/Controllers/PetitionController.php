6196297e8b88b2dc53e1e564d993d1012c590fdd
JuanMatheusDosSantos <daw04.2025.jesuitas@gmail.com>
2025-11-25 13:50:25 +0100

    25/11 13:50

--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Petition;
 use Illuminate\Http\Request;
 
+
 class PetitionController extends Controller
 {
     function index()
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PetitionController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +19,21 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get("/", [PagesController::class, "home"])->name("home");
+Route::get("/users/firmas", [UserController::class, "peticionesFirmadas"])->middleware("auth");
 
+Route::controller(PetitionController::class)->group(function () {
+    Route::get("petitions/index", "index")->name("peticiones.index");
+    Route::get("mypetitions", "listMine")->name("peticiones.mine");
+    Route::get("petitions", "signedpetitions")->name("peticiones.peticionesfirmadas");
+
+    Route::get("petitions/{id}", "show")->name("peticiones.show");
+    Route::get("petitions/add", "create")->name("peticiones.create");
+    Route::post("petition", "store")->name("petiticiones.store");
+    Route::delete("petition/{id}", "delete")->name("peticiones.delete");
+    Route::put('petition/{id}', 'update')->name('peticiones.update');
+    Route::post('petition/sign/{id}', 'sing')->name('peticiones.firmar');
+    Route::get('petition/edit/{id}', 'update')->name('peticiones.edit');
+
+});
 require __DIR__.'/auth.php';
